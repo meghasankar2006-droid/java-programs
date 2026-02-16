8f633058b540bf0b6aeedb3d3307a7b07425d1b2
Meghasri <meghasankar2006@gmail.com>
2026-02-16 13:39:50 +0530

    Add files via upload

---
+++ intoduction/Prg13.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Prg13 {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int yieldPerAcre=sc.nextInt();
+       int acres=sc.nextInt();
+       int extraYield=sc.nextInt();
+       int damagedCrops=sc.nextInt();
+       int tot=(yieldPerAcre*acres)+extraYield-damagedCrops;
+       System.out.println(tot);
+
+
+    }
+}

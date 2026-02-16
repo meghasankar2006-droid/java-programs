61e9765ab88630f3ef96c679b3e42f9bbe8f391d
Meghasri <meghasankar2006@gmail.com>
2026-02-16 13:38:15 +0530

    Add files via upload

---
+++ intoduction/Prg8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Prg8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int rodsPerHour=sc.nextInt();
+        int hoursWorked=sc.nextInt();
+        int overtimeRods=sc.nextInt();
+        int rejectedRods=sc.nextInt();
+        int wrk=rodsPerHour*hoursWorked;
+        int tot=(wrk+overtimeRods)-rejectedRods;
+        System.out.println(tot);
+
+    }
+}

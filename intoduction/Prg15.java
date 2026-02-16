5f7f373d49039093d45221be6978bb7f7ff96df3
Meghasri <meghasankar2006@gmail.com>
2026-02-16 13:40:16 +0530

    Add files via upload

---
+++ intoduction/Prg15.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Prg15 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int rooms=sc.nextInt();
+        int price=sc.nextInt();
+        int serviceCharges=sc.nextInt();
+        int commission=sc.nextInt();
+        int revenue=(rooms*price)+serviceCharges-commission;
+        System.out.println(revenue);
+    }
+}

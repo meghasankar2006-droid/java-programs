1aa70111c7f41144b4737cf4e3f33332d7db4d7f
Meghasri <meghasankar2006@gmail.com>
2026-02-17 09:15:37 +0530

    Add files via upload

---
+++ Operators/Prg9.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Prg9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int user=sc.nextInt();
+        int pass=sc.nextInt();
+        int acc=sc.nextInt();
+        if(user==1 && pass==1 && acc==0){
+            System.out.println("Login Success");
+        }
+        else{
+            System.out.println("Login Failed");
+        }
+    }
+}

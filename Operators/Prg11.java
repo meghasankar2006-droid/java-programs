761e9ae6526ee5a0f22881c541f1e0da8f3d194d
Meghasri <meghasankar2006@gmail.com>
2026-02-18 09:19:27 +0530

    Add files via upload

---
+++ Operators/Prg11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Prg11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int base=sc.nextInt();
+        int lev=sc.nextInt();
+        int cap=(int)Math.pow(2,lev);
+        int ans=base*cap;
+        System.out.println(ans);
+        
+    }
+}

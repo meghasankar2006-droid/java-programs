a6920968b136248c58a49f04ff9aeef9bbdae3eb
Meghasri <meghasankar2006@gmail.com>
2026-02-18 23:41:54 +0530

    Add files via upload

---
+++ Operators/Prg14.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Prg14 {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int tot=a|b;
+    System.out.println(tot);
+}  
+}

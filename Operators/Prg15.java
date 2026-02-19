d2ce4272c6a07a96ca9906efddde0710dea114d6
Meghasri <meghasankar2006@gmail.com>
2026-02-19 09:34:13 +0530

    Update Prg15.java

--- Operators/Prg15.java
+++ Operators/Prg15.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
+public class Prg {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int num=a^b;
+      System.out.println(num);  
+    }
+}
 

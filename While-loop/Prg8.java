aa013ceaf7466e93ef763335c4f5e0a48edebd2f
Meghasri <meghasankar2006@gmail.com>
2026-03-06 09:58:58 +0530

    Add files via upload

---
+++ While-loop/Prg8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Prg8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println("Number: "+num);
+        int sum=0;
+        while(num>0){
+            int rem=num%10;
+            sum=sum+rem;
+            num=num/10;
+        }
+        System.out.println("Sum of digits: "+sum);
+    }
+}

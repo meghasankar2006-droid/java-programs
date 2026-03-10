9835b03e58a86fa3d74bbd83e6f9c4f25a664453
Meghasri <meghasankar2006@gmail.com>
2026-03-10 22:15:16 +0530

    Add files via upload

---
+++ Nested-forloop/Prg1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Prg1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        for(int i=1;i<=r;i++){
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+        for(int i=1;i<=r;i++){
+            System.out.print(i+" ");
+            for(int j=1;j<=c;j++){
+                System.out.print(i*j+" ");
+            }
+            System.out.println("");
+        }
+    }
+}

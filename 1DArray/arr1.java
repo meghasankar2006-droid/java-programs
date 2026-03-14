b465c04bfc3745b882f927590907ae376a35e555
Meghasri <meghasankar2006@gmail.com>
2026-03-14 23:11:50 +0530

    Add files via upload

---
+++ 1DArray/arr1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class arr1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        double sum=0.0;
+        int acount=0;
+        double []arr=new double[num];
+        for(int i=0;i<num;i++){
+            arr[i]=sc.nextDouble();
+            sum=sum+arr[i];
+            
+        }
+        double avg=sum/num;
+
+        for(int i=0;i<num;i++){
+            if(arr[i]>avg){
+                acount++;
+            }
+        }
+        double per=(acount*100.0)/num;
+        System.out.println("Total Employees: "+num);
+        System.out.printf("Average Salary: %.2f  \n ",avg);
+        System.out.println("Employees Above Average: "+acount);
+        System.out.printf("Percentage: %.2f%% \n",per);
+        
+
+    }
+}

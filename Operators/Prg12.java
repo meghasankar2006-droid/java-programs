456644823b7d50c92fd6fd3c0918c64ff43adada
Meghasri <meghasankar2006@gmail.com>
2026-08-09 23:48:39 +0530

    Format code in Prg12.java for consistency

--- Operators/Prg12.java
+++ Operators/Prg12.java
@@ -3,6 +3,7 @@ import java.math.*;
 public class Prg12 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        
         int size=sc.nextInt();
         int cycle=sc.nextInt();
         int p=(int)Math.pow(2,cycle);
